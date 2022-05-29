81acb1393c55d54e7328057aeb4d616496bfc051
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-05-29 13:47:23 +0900

    feat(variables): define getPower() function

--- variables/main.go
+++ variables/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	power := 9000
-	fmt.Printf("It's over %d\n", power)
+	power := getPower()
+	fmt.Printf("It's over %d!\n", power)
+}
+
+func getPower() int {
+	return 9001
 }
